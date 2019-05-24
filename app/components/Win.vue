3fde9d88f9ee9e5178de1094700b2fe39993385d
unknown <felix.robaglia@gmail.com>
2019-05-24 22:50:03 +0200

    add stats on win screen

--- app/components/Win.vue
+++ app/components/Win.vue
@@ -2,7 +2,7 @@
   <div class="big-header">
     <strong class="won"> Vous avez gagné ! </strong>
     <img style="width: 40%" src="../assets/img/allmight.gif">
-    <h1 style="margin-top: 30px">{{ message }}</h1>
+    <h1 style="margin-top: 30px"> Vous avez sauvé le village, bravo ! Vous finissez la partie avec <span class="gold"> {{ gameService.gold }} pièces d'or </span> et <span class="health"> {{ gameService.stats.health }} / {{ gameService.maxHealth }} points de vie </span>. Essayez de faire mieux !</h1>
     <br />
     <router-link v-on:click.native="gameService.resetData()" class="button" to="./home">Rejouer</router-link>
   </div>
@@ -24,8 +24,7 @@ import gameService from '../services/gameService.js'
 export default {
   data() {
     return {
-      gameService: gameService,
-      message: "Vous avez sauvé le village, bravo !",
+      gameService: gameService
     };
   }
 };
